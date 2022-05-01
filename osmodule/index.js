7ee58cf3e5b4d0c94f3e0bbe712b749c3aa831b6
Cloudy_minded <69113818+SupratimSarkar2001@users.noreply.github.com>
2022-05-01 20:12:29 +0530

    Day 2 Learing

---
+++ osmodule/index.js
@@ -0,0 +1,16 @@
+console.log("Hello Pixxa 🐱‍🐉")
+
+const os= require("os");
+
+//give us how many bit our computer is
+console.log(os.arch());
+
+//freemem
+console.log(os.freemem());
+
+//host name
+console.log(os.hostname());
+
+console.log(os.platform());
+
+console.log(os.cpus());
